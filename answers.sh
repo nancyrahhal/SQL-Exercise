b1a1dd3e62736da6b8e048fc9a25428609e80770
Nancy Rahhal <nancyrahhal97@gmail.com>
2023-09-19 00:19:20 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -1,34 +1,65 @@
 
 
-//ex1
+# ex1
 SELECT Name 
 FROM students;
 
-//ex2
+# ex2
 SELECT * 
 FROM students 
 WHERE Age>30;
 
-//ex3
+# ex3
 SELECT Name 
 FROM students 
 WHERE Gender="F" AND Age=30;
 
-//ex4
+# ex4
 SELECT Points 
 FROM students 
 WHERE Name="Alex";
 
-//ex5
+# ex5
 INSERT INTO students (ID, Name, Age, Gender, Points)
 VALUES (7, "Nancy", 26, "F", 900);
 
-//ex6
+# ex6
 UPDATE students
 SET Points=Points+40
 WHERE Name="Basma";
 
-//ex7
+#ex7
 UPDATE students
 SET Points=Points-40
 WHERE ID=1;
+
+# ex8
+# //done
+
+# ex9
+# //done
+
+#Creating table graduate
+CREATE TABLE graduates (
+    ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL UNIQUE,
+    Age INTEGER,
+    Gender TEXT,
+	Points INTEGER,
+	Graduation TEXT
+);
+
+# ex10
+INSERT INTO graduates(Name, Age, Gender, Points)
+SELECT Name, Age, Gender, Points
+FROM students
+WHERE Name="Layal";
+
+# ex11
+UPDATE graduates
+SET Graduation="08/09/2018"
+WHERE Name="Layal";
+
+# ex12
+DELETE FROM students
+WHERE Name="Layal";
